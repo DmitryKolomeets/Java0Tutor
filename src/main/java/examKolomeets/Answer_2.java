3198f0ebc1047e74b3402241eb8f159817469942
Dmitry Kolomeets <bde@ukr.net>
2022-08-21 20:49:48 +0300

    init commit

---
+++ src/main/java/examKolomeets/Answer_2.java
@@ -0,0 +1,17 @@
+package examKolomeets;
+
+public class Answer_2 {
+
+    public static int pow(int x, int n) {
+        int result = 1;
+        for (int i = 1; i <= n; i++) {
+            result = result * x;
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(Answer_2.pow(7, 4));
+    }
+
+}
