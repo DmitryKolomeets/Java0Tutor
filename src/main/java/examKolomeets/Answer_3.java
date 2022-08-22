64720a75c9aebf721eba2cf51cba1d2ab02199e3
Dmitry Kolomeets <bde@ukr.net>
2022-08-22 23:25:45 +0300

    init commit

---
+++ src/main/java/examKolomeets/Answer_3.java
@@ -0,0 +1,27 @@
+package examKolomeets;
+
+public class Answer_3 {
+
+
+
+
+        public static void main(String[] args) {
+
+            int []a = {7, 6, 5, 4, 3, 2, 1};
+
+            int i = a.length - 1;
+
+            while (i > 1) {
+
+                i-=2;
+
+                System.out.print(a[i]);
+
+            }
+
+
+
+
+
+        }
+}
